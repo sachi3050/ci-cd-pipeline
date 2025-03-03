bd16d9b27cec78bb3c62cd863123143b2e0738d5
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-04 02:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Future DevOps Engineers! Your CI/CD pipeline is working."
+    return "Hello, Honey : My Brest Friend, Whould You wanna Join Tonight"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
