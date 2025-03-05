3d4afae4751ab114150138c3a4191dbf9f38e205
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-05 23:32:38 +0530

    Update app.py (wish)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Honey : My Brest Friend, Whould You wanna Join Tonight"
+    return "Happy BirthDay Abhishek Bhaina. Haste rahiye muskurate rahiye, All the Very Best......"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
