fcd198a09f3ebe1e9ec934b35d14706da9a20324
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-06 02:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def home():
@@ -10,6 +10,9 @@ def home():
     <img src="/static/image.jpg" alt="Birthday Image" width="400">
     '''
 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
