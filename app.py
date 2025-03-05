1d224e88449798683360558e7396a732a4067c9a
sachi3050 <sabarsachidananda@gmail.com>
2025-03-06 00:46:38 +0530

    Staged unstaged changes

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+<<<<<<< HEAD
     return "Happy BirthDay Abhishek Bhaina. Haste rahiye muskurate rahiye, All the Very Best......"
+=======
+    return '''
+    <h2>Happy Birthday Abhishek Bhaina!</h2>
+    <p>Haste rahiye, muskurate rahiye, All the Very Best...</p>
+    <img src="/static/image.jpg" alt="Birthday Image" width="400">
+    '''
+>>>>>>> 032db44 (Staged unstaged changes)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
