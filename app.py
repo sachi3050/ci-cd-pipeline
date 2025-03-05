3171fc03f8421ebfb192e7d2d0524ac94ca8a840
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-06 03:11:03 +0530

    Update app.py (Indentation)

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return '''
+    return """ 
     <h2>Happy Birthday Abhishek Bhaina!</h2>
     <p>Haste rahiye, muskurate rahiye, All the Very Best...</p>
     <img src="/static/image.jpg" alt="Birthday Image" width="400">
-    '''
-
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(app.static_folder, filename)
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
