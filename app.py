2cd4c067724f77ad75175041619f85e118d76931
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-06 19:58:15 +0530

    Update app.py (new Ip check)

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def home():
     <p>Haste rahiye, muskurate rahiye, All the Very Best...</p>
     <img src="/static/image.jpg" alt="Birthday Image" width="400">
     """
-
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
