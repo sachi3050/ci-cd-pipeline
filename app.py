0de7b18fae9f70a6f0b30baed4375fbe7edce040
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2025-03-07 14:39:30 +0530

    Update app.py (Final Version)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """ 
-    <h2>Happy Birthday Abhishek Bhaina!</h2>
+    <h2>Happy Birthday Bixi Boss👑🎁🎂!</h2>
     <p>Haste rahiye, muskurate rahiye, All the Very Best...</p>
     <img src="/static/image.jpg" alt="Birthday Image" width="400">
     """
